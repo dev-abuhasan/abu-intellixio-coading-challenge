d87a5a11a29c8dab6d76ee18841144ff8636da24
dev-abuhasan <ahinstitutte@gmail.com>
2024-11-11 16:53:15 +0600

    Fix: Implemented challenges

--- src/views/products/products.tsx
+++ src/views/products/products.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useEffect } from "react";
 import { Product } from "@/types";
 import { ProductModal } from "@/views/products/productModal/productModal";
 import { BackToHome } from "@/components/backToHome/backToHome";
@@ -8,8 +8,17 @@ import { ProductList } from "@/views/products/productList/productList";
 import { PaginationControls } from "@/views/products/paginationControls/paginationControls";
 import { usePagination } from "@/hooks/usePagination";
 import { PRODUCTS_DATA } from "@/data/productsData";
+import { usePathname, useRouter } from "next/navigation";
 
-export const Products: React.FC = () => {
+type SearchParams = { [key: string]: string | string[] | undefined };
+interface ProductsProps {
+  searchParams: SearchParams;
+}
+
+export const Products: React.FC<ProductsProps> = ({ searchParams }) => {
+  const router = useRouter();
+  const pathName = usePathname();
+
   const [selectedProduct, setSelectedProduct] = useState<Product | null>(null);
   const {
     currentPage,
@@ -18,12 +27,30 @@ export const Products: React.FC = () => {
     handlePageChange,
   } = usePagination({ items: PRODUCTS_DATA, itemsPerPage: 5 });
 
+  useEffect(() => {
+    if (searchParams?.['product-id']) {
+      const currentProduct = paginatedProducts.find(product => product.id === searchParams['product-id']);
+      if (currentProduct) {
+        setSelectedProduct(currentProduct);
+      } else {
+        setSelectedProduct(null);
+      }
+    } else if (selectedProduct !== null) {
+      setSelectedProduct(null);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [paginatedProducts, searchParams]);
+
   const handleOpenModal = useCallback((product: Product) => {
+    router.push(`${pathName}?product-id=${product.id}`);
     setSelectedProduct(product);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const handleCloseModal = useCallback(() => {
+    router.push(`${pathName}`);
     setSelectedProduct(null);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
